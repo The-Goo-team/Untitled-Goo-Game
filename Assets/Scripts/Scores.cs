20cb436872ef18fabf41e24601d910ee52f453c2
JustAlejandro <32211773+JustAlejandro@users.noreply.github.com>
2019-09-21 11:06:54 -0500

    Replenshing in Place
    When heart hits npGoo, npGoo and heart are replaced by Player goo. Attached Micah's sprites to player, heart, and splat

---
+++ Assets/Scripts/Scores.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scores : MonoBehaviour
+{
+    //Holds the max amount of lives for the level
+    public int livesleft;
+    private int lives;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/regenScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class regenScript : MonoBehaviour
+{
+    //Stores the Rigidbody of the Heart
+    private Rigidbody2D rigBod;
+    public GameObject gooBody;
+    private bool createdBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting the rigidBody
+        rigBod = GetComponent<Rigidbody2D>();
+        createdBody = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("npGoo") && !createdBody) {
+            createdBody = true;
+            Destroy(other.gameObject);
+            Instantiate(gooBody, other.gameObject.transform.position, Quaternion.identity);
+            /* This code doesn't work, trying to keep the velocity, maybe come backa nd fix later
+            GameObject gooBaby = gooBody.transform.Find("heart").gameObject;
+            if (gooBaby == null) { Debug.Log("No child found"); }
+            gooBaby.GetComponent<Rigidbody2D>().AddForce(rigBod.velocity * 100000, ForceMode2D.Impulse);
+            */
+            Destroy(this.gameObject);
+        }
+    }
+}
