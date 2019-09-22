61ee59d7d2a5abb71b3e16585a3538fc31454d09
Marco Ravelo <mravelo5874@gmail.com>
2019-09-21 19:38:41 -0500

    fixed merge stuffs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject flyingGoo;
-<<<<<<< HEAD
-    public Crosshair    reticle;
-    public float   TotalTime=1.0f;
-    float          CurrentTime=0;
-    Vector2        mousePosition;
-    Vector2         playerPos;
-    float           lineLength;
-    Vector2         Dir;
-    GameObject      fly_goo;
-=======
     public GameObject heart_projectile;
     public GameObject heart;
->>>>>>> 5dea35d2f700eca78add7f5d8b72a7c1f216f016
+
 
     private void Start()
     {
-        reticle = FindObjectOfType<Crosshair>();
     }
     private void Update()
     {
@@ -30,32 +19,6 @@ public class PlayerController : MonoBehaviour
         {
             dropScore();
             Vector3 spawn_pos = this.transform.position;
-<<<<<<< HEAD
-            spawn_pos.y += 1;
-            fly_goo = Instantiate(flyingGoo, spawn_pos, Quaternion.identity);
-            mousePosition = Camera.main.ScreenToWorldPoint( reticle.GetMousePositionOnClick());
-            playerPos = new Vector2(GameObject.Find("Player").transform.position.x, GameObject.Find("Player").transform.position.y);
-            lineLength = (mousePosition - playerPos).magnitude; // Length of each line segment
-            Dir=(mousePosition-playerPos).normalized;
-            
-
-
-        }
-        Vector2 Force=new Vector2(0,0);
-        if(CurrentTime<TotalTime && fly_goo!=null)
-        {
-            CurrentTime+=Time.deltaTime;
-            Vector2 nextPosLin   = playerPos+(Dir *lineLength*( CurrentTime/TotalTime));
-            float nextPosy       = Mathf.Sin(((float)CurrentTime/(float)TotalTime)* 120.0f * Mathf.Deg2Rad)*nextPosLin.y+playerPos.y;
-            Force=new Vector2( nextPosLin.x, nextPosy<0.0f?nextPosy*-1:nextPosy);
-            fly_goo.GetComponent<Rigidbody2D>().velocity=(Force);
-
-        }
-        else{
-            CurrentTime=0;
-            fly_goo=null;
-
-=======
             Vector3 mouse_pos = Input.mousePosition;
             mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
             
@@ -83,7 +46,6 @@ public class PlayerController : MonoBehaviour
             fly_heart.GetComponent<heartController>().original_player_refrence = this.gameObject;
             fly_heart.GetComponent<heartController>().FlyTo(mouse_pos);
             Destroy(heart.gameObject);
->>>>>>> 5dea35d2f700eca78add7f5d8b72a7c1f216f016
         }
     }
 
