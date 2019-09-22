841a1e580910bf28b7d09ceeb4d8b6a9ad2be652
Marco Ravelo <mravelo5874@gmail.com>
2019-09-21 19:48:29 -0500

    launch_site addded to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,32 +7,26 @@ public class PlayerController : MonoBehaviour
     public GameObject flyingGoo;
     public GameObject heart_projectile;
     public GameObject heart;
+    public Transform launch_site;
 
-
-    private void Start()
-    {
-    }
     private void Update()
     {
         // throw goo
         if (Input.GetMouseButtonDown(0))
         {
             dropScore();
-            Vector3 spawn_pos = this.transform.position;
             Vector3 mouse_pos = Input.mousePosition;
             mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
             
             mouse_pos -= this.transform.position;
             mouse_pos.Normalize();
-            spawn_pos += mouse_pos;
-            spawn_pos.y += 1f;
-            GameObject fly_goo = Instantiate(flyingGoo, spawn_pos, Quaternion.identity);
+
+            GameObject fly_goo = Instantiate(flyingGoo, launch_site.position, Quaternion.identity);
             fly_goo.GetComponent<flyingGooController>().FlyTo(mouse_pos);
         }
         // throw heart
         if (Input.GetMouseButtonDown(1))
         {
-            Vector3 spawn_pos = this.transform.position;
             Vector3 mouse_pos = Input.mousePosition;
             //FindObjectsOfTypeAll The mitochondria is the powerhouse of the cell
             mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
@@ -40,9 +34,7 @@ public class PlayerController : MonoBehaviour
             
             mouse_pos -= this.transform.position;
             mouse_pos.Normalize();
-            spawn_pos += mouse_pos;
-            spawn_pos.y += 1f;
-            GameObject fly_heart = Instantiate(heart_projectile, spawn_pos, Quaternion.identity);
+            GameObject fly_heart = Instantiate(heart_projectile, launch_site.position, Quaternion.identity);
 
             fly_heart.GetComponent<heartController>().original_player_refrence = this.gameObject;
             fly_heart.GetComponent<heartController>().FlyTo(mouse_pos);
