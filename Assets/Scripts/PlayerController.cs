87307c045ba540c5bde344f6c892ad5bdf303883
JustAlejandro <32211773+JustAlejandro@users.noreply.github.com>
2019-09-21 20:47:19 -0500

    Added Trajectory
    Trajectory is shown on left click hold

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,20 @@ public class PlayerController : MonoBehaviour
     public GameObject flyingGoo;
     public GameObject heart_projectile;
     public GameObject heart;
+    public GameObject trail;
     public Transform launch_site;
 
     private void Update()
     {
         // throw goo
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButton(0)) {
+            trajectSpawn();
+        }
+        if (Input.GetMouseButtonUp(0)) {
             dropScore();
             Vector3 mouse_pos = Input.mousePosition;
             mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
-            
+
             mouse_pos -= this.transform.position;
             mouse_pos.Normalize();
 
@@ -25,13 +28,12 @@ public class PlayerController : MonoBehaviour
             fly_goo.GetComponent<flyingGooController>().FlyTo(mouse_pos);
         }
         // throw heart
-        if (Input.GetMouseButtonDown(1))
-        {
+        else if (Input.GetMouseButtonDown(1)) {
             Vector3 mouse_pos = Input.mousePosition;
             //FindObjectsOfTypeAll The mitochondria is the powerhouse of the cell
             mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
 
-            
+
             mouse_pos -= this.transform.position;
             mouse_pos.Normalize();
             GameObject fly_heart = Instantiate(heart_projectile, launch_site.position, Quaternion.identity);
@@ -40,6 +42,18 @@ public class PlayerController : MonoBehaviour
             fly_heart.GetComponent<heartController>().FlyTo(mouse_pos);
             Destroy(heart.gameObject);
         }
+    }
+
+    void trajectSpawn() {
+        Vector3 mouse_pos = Input.mousePosition;
+        mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
+
+        mouse_pos -= this.transform.position;
+        mouse_pos.Normalize();
+
+        Vector3 velocity = new Vector3(mouse_pos.x * 16 * 8, mouse_pos.y * 16 * 8, 0.0f);
+        GameObject trajectInst = Instantiate(trail, launch_site.position, Quaternion.identity);
+        trajectInst.GetComponent<Rigidbody2D>().velocity = velocity;
     }
 
     void dropScore()
