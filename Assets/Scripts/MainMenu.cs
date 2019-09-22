f9d23f7e97bb2f5044cd3b1b3d3421c41c2804a0
Allan <allanfrederick1224@utexas.edu>
2019-09-21 23:11:05 -0500

    reticle

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,29 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static Crosshair;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotspot;
 
 public AudioSource MusicSource;
 
 public AudioSource PlayClick;
 
+
+
 void Start(){
 
-
     MusicSource.Play();
+    hotspot = new Vector2(cursorTexture.width/2, cursorTexture.height/2);
+    Cursor.SetCursor(cursorTexture, hotspot, cursorMode);
 }
+
+ 
+
+
 
    public void PlayGame(){
        
        PlayClick.Play();
         // Load next scene in queue
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-       
-       
-       
    }
 
     public void QuitGame(){
