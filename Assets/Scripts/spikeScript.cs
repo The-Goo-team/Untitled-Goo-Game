55f9b0a5a2bec32990b5ee547ae696b90a9f5d57
Marco Ravelo <mravelo5874@gmail.com>
2019-09-21 20:48:35 -0500

    spikes

---
+++ Assets/Scripts/spikeScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.GetComponent<flyingGooController>() != null) {
+            Destroy(other.gameObject);
+        }
+    }
+}
